33966d1790af28b2943f67ec989620d9e3492cb5
Pavel Chervakov <chervakov@gmail.com>
2017-07-17 19:43:34 +0200

    Add 'remove background' functionality

--- src/nehuba/internal/nehuba_renderers.ts
+++ src/nehuba/internal/nehuba_renderers.ts
@@ -5,6 +5,8 @@ import {GL} from 'neuroglancer/webgl/context';
 import {ShaderBuilder, ShaderModule, ShaderProgram} from 'neuroglancer/webgl/shader';
 import {getSquareCornersBuffer} from 'neuroglancer/webgl/square_corners_buffer';
 
+import { removeBackgroundMode } from "shuba/config";
+
 /**
  * In neuroglancer's SliceViewRenderHelper the shader is built in constructor. So it is not feasible to extend or monkey-patch it. 
  * Therefore the fork of the whole SliceViewRenderHelper class is needed to change it.
@@ -24,7 +26,9 @@ export class NehubaSliceViewRenderHelper extends RefCounted {
 
   private textureCoordinateAdjustment = new Float32Array(4);
 
-  constructor(public gl: GL, emitter: ShaderModule) {
+  discardColor = vec4.fromValues(0.5, 0.5, 0.5, 1);
+
+  constructor(public gl: GL, emitter: ShaderModule, mode: removeBackgroundMode) {
     super();
     let builder = new ShaderBuilder(gl);
     builder.addVarying('vec2', 'vTexCoord');
@@ -36,14 +40,24 @@ export class NehubaSliceViewRenderHelper extends RefCounted {
     builder.addUniform('vec4', 'uBackgroundColor');
     builder.addUniform('mat4', 'uProjectionMatrix');
     builder.addUniform('vec4', 'uTextureCoordinateAdjustment');
+    builder.addUniform('vec4', 'uDiscardColor');
     builder.require(emitter);
-    builder.setFragmentMain(`
+let originalFragment = `
 vec4 sampledColor = texture2D(uSampler, vTexCoord);
 if (sampledColor.a == 0.0) {
   sampledColor = uBackgroundColor;
 }
 emit(sampledColor * uColorFactor, vec4(0,0,0,0));
-`);
+`;
+let nehubaFragment = `
+vec4 sampledColor = texture2D(uSampler, vTexCoord);
+if (sampledColor.a == 0.0) {
+  sampledColor = uBackgroundColor;
+}
+if (sampledColor.r ${mode} uDiscardColor.r && sampledColor.g ${mode} uDiscardColor.g && sampledColor.b ${mode} uDiscardColor.b) discard;
+else emit(sampledColor * uColorFactor, vec4(0,0,0,0));
+`;
+    builder.setFragmentMain(mode === 'none' ? originalFragment : nehubaFragment);
     builder.addAttribute('vec4', 'aVertexPosition');
     builder.setVertexMain(`
 vTexCoord = uTextureCoordinateAdjustment.xy + 0.5 * (aVertexPosition.xy + 1.0) * uTextureCoordinateAdjustment.zw;
@@ -67,6 +81,7 @@ gl_Position = uProjectionMatrix * aVertexPosition;
     gl.uniform4fv(shader.uniform('uColorFactor'), colorFactor);
     gl.uniform4fv(shader.uniform('uBackgroundColor'), backgroundColor);
     gl.uniform4fv(shader.uniform('uTextureCoordinateAdjustment'), textureCoordinateAdjustment);
+    gl.uniform4fv(shader.uniform('uDiscardColor'), this.discardColor);
 
     let aVertexPosition = shader.attribute('aVertexPosition');
     this.copyVertexPositionsBuffer.bindToVertexAttrib(aVertexPosition, /*components=*/2);
@@ -77,9 +92,9 @@ gl_Position = uProjectionMatrix * aVertexPosition;
     gl.bindTexture(gl.TEXTURE_2D, null);
   }
 
-  static get(gl: GL, emitter: ShaderModule) {
+  static get(gl: GL, emitter: ShaderModule, mode: removeBackgroundMode) {
     return gl.memoize.get(
-        `nehuba/NehubaSliceViewRenderHelper:${getObjectId(emitter)}`,
-        () => new NehubaSliceViewRenderHelper(gl, emitter));
+        `nehuba/NehubaSliceViewRenderHelper:${getObjectId(emitter)}:${mode}`,
+        () => new NehubaSliceViewRenderHelper(gl, emitter, mode));
   }
 }
