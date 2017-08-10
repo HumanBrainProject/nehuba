d977da8b2da9a7bfbd6452cea55723b98ebabbab
Pavel Chervakov <chervakov@gmail.com>
2017-08-10 17:37:06 +0200

    Add errorHandler and segment enter/leave callbacks

--- src/nehuba/NehubaViewer.ts
+++ src/nehuba/NehubaViewer.ts
@@ -3,23 +3,64 @@ import { Viewer } from "neuroglancer/viewer";
 
 import { Config } from "nehuba/config";
 import { patchNeuroglancer } from "nehuba/internal/patches";
-import { configureInstance, configureParent, forEachSegmentationUserLayerOnce, disableSegmentSelectionForLayer } from "nehuba/internal/hooks";
+import { configureInstance, configureParent, forEachSegmentationUserLayerOnce, disableSegmentSelectionForLayer, forAllSegmentationUserLayers } from "nehuba/internal/hooks";
 import { configSymbol } from "nehuba/internal/nehuba_layout";
 
 /** Create viewer */
-export function createNehubaViewer(configuration?: Config/* , container?: HTMLElement */) { //TODO Accept String id for container and lookup ElementById
-	return NehubaViewer.create(configuration/* , container */);
+export function createNehubaViewer(configuration?: Config/* , container?: HTMLElement */, errorHandler?: (error: Error) => void) { //TODO Accept String id for container and lookup ElementById
+	return NehubaViewer.create(configuration/* , container */, errorHandler);
 }
 
 export class NehubaViewer {
 	/** Don't use it, should be private. Left exposed just in case you urgently need it and there is no way around it.
 	 *  But be warned that you can easily brake things by accessing it directly. */
-	ngviewer: Viewer
+	readonly ngviewer: Viewer
 	private _config: Config;
 
-	private constructor(viewer: Viewer, config: Config) {
+	private mouseOnSegment?: (segment: number, layer?: {name: string, url:string}) => void;
+	private mouseOffSegment?: () => void;
+
+	setMouseEnterSegmentCallback(callback: (segment: number, layer?: {name: string, url:string}) => void) {
+		this.mouseOnSegment = callback;
+	}
+	clearMouseEnterSegmentCallback() {
+		this.mouseOnSegment = undefined;
+	}
+	setMouseLeaveSegmentCallback(callback: () => void) {
+		this.mouseOffSegment = callback;
+	}
+	clearMouseLeaveSegmentCallback() {
+		this.mouseOffSegment = undefined;
+	}
+
+	private constructor(viewer: Viewer, config: Config, public errorHandler?: (error: Error) => void) {
 		this.ngviewer = viewer;
 		this._config = config;
+
+		const callbacksSet = Symbol('Callbacks are set');
+		forAllSegmentationUserLayers(viewer, layer => {
+			if ((<any>layer)[callbacksSet]) return;
+			const selection = layer.displayState.segmentSelectionState;
+			selection.registerDisposer(selection.changed.add(() => {
+				const {mouseOnSegment, mouseOffSegment} = this;
+				if (selection.hasSelectedSegment) {
+					const selected = selection.selectedSegment;
+					if (selected.high !== 0) this.handleError('Segment id number does not fit into 32 bit integer');
+					const segment = selected.low;
+					/* if (segment) */ mouseOnSegment && mouseOnSegment(segment);
+					// else mouseOffSegment && mouseOffSegment();
+				} else mouseOffSegment && mouseOffSegment();
+			}));
+			(<any>layer)[callbacksSet] = true;
+		});
+	}
+
+	private handleError(message: string) {
+		const error = new Error(message);
+		const {errorHandler} = this;
+		// this.errorHandler ? this.errorHandler(error) :  (() => {throw error})();
+		errorHandler && errorHandler(error);
+		throw error;
 	}
 
 	get config() { return this._config; }
@@ -29,11 +70,16 @@ export class NehubaViewer {
 		(this.ngviewer.display.container as any)[configSymbol] = this._config;
 	}
 
-	static create(configuration?: Config/* , container?: HTMLElement */) { //TODO Accept String id for container and lookup ElementById
+	static create(configuration?: Config/* , container?: HTMLElement */, errorHandler?: (error: Error) => void) { //TODO Accept String id for container and lookup ElementById
 		const config = configuration || {};
 
 		const parent = /* container ||  */document.getElementById('container')!; //TODO id as param ( String|HTMLElement )
-		if ((<any>parent)[configSymbol]) throw new Error('Viewer is already created in this container: ' + parent);
+		if ((<any>parent)[configSymbol]) {
+			const error = new Error('Viewer is already created in this container: ' + parent);
+			// errorHandler ? errorHandler(error) :  (() => {throw error})();
+			errorHandler && errorHandler(error);
+			throw error;
+		}
 		(<any>parent)[configSymbol] = config;
 
 		patchNeuroglancer(config);
@@ -47,7 +93,7 @@ export class NehubaViewer {
 			NehubaViewer.restoreInitialState(viewer, config);
 		}
 
-		return new NehubaViewer(viewer, config);
+		return new NehubaViewer(viewer, config, errorHandler);
 	}
 	private static restoreInitialState(viewer: Viewer, config: Config) {
 		const state = config.dataset && config.dataset.initialNgState;
